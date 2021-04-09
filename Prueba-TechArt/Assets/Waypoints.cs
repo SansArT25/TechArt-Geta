74168df0bc92211bf164e54ef3664b59c65b2ab0
SansArT <54065229+SansArT25@users.noreply.github.com>
2021-04-08 23:46:41 -0500

    Final Version

---
+++ Prueba-TechArt/Assets/Waypoints.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+    [SerializeField] private GameObject[] waypoints;
+    [SerializeField] private float speed;
+    private Vector3 currentPos, targetPos;
+    private int currentWaypoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentPos = transform.position;
+        targetPos = waypoints[0].transform.position;
+        currentWaypoint = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position != targetPos)
+        {
+            //transform.position = Vector3.Lerp(transform.position, targetPos,);
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, speed);
+        }
+        else if (currentWaypoint < waypoints.Length -1)
+        {
+            currentWaypoint++;
+            targetPos = waypoints[currentWaypoint].transform.position;
+        }
+    }
+}
